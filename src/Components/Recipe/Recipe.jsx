adf7746c77c284cc3d8f9f27238b0bf8f232312c
Emon Arefin <emon360degree@gmail.com>
2023-05-03 01:18:47 +0600

    Chef Data Loaded

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import RecipePage from './RecipePage';
 
 const Recipe = () => {
     const { id } = useParams();
+
+    const [chefs, setChefs] = useState([])
+
+    useEffect(() => {
+        fetch('https://assignment-10-chef-server-emon360arefin.vercel.app/chef')
+            .then(res => res.json())
+            .then(data => setChefs(data))
+    }, [])
+
+    const recipe = chefs.filter(chef => chef.id === parseInt(id)  )
+
     return (
         <div>
-            <h2>Recipe: {id}</h2>
+            {
+                recipe && <RecipePage key={recipe.id} recipe={recipe}></RecipePage>
+            }
         </div>
     );
 };
---
+++ src/Components/Recipe/RecipePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RecipePage = (props) => {
+    const {} = props.recipe
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RecipePage;
