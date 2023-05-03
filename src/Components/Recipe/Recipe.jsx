9866bab1414e5fb96c1e06f4115ee9d925afe7fc
Emon Arefin <emon360degree@gmail.com>
2023-05-04 02:22:58 +0600

    Recipe Page Title Updated

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -34,7 +34,7 @@ const Recipe = () => {
             </div>
             <div className=' bg-[#FFFAFA] py-6 md:py-16 px-2'>
                 <div className='max-w-7xl mx-auto'>
-                    <h2 className='text-4xl text-center md:text-4xl font-bold leading-snug text-transparent bg-clip-text bg-gradient-to-r from-[#EA1E34] to-[#EB1555]'>Our Chefs Specials</h2>
+                    <h2 className='text-4xl text-center md:text-4xl font-bold leading-snug text-transparent bg-clip-text bg-gradient-to-r from-[#EA1E34] to-[#EB1555]'>{chef && chef.name}'s Specials</h2>
 
                     <h2 className='text-lg text-center text-slate-600 my-6 leading-snug'>In this exclusive section, our skilled and innovative chefs showcase their culinary expertise through a collection of extraordinary dishes known as the "Chef Special Recipes." These recipes are meticulously crafted with a combination of unique flavors, premium ingredients, and creative techniques, ensuring an unforgettable dining experience.</h2>
                 </div>
