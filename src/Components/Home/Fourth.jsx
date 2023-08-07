e715f1847800b73953e48bd468f1d7e8a7cf7d08
Emon Arefin <emon360degree@gmail.com>
2023-08-08 01:04:34 +0600

    Card Path Removed

--- src/Components/Home/Fourth.jsx
+++ src/Components/Home/Fourth.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Marquee from "react-fast-marquee";
-import CommunityCard from './communityCard';
 
 const Fourth = () => {
     return (
